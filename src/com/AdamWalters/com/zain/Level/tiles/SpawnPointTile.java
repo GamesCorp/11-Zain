57802697a3680a13cac05988ffac86dcee1b2c06
GamesCorp <ad-walters@sky.com>
2013-03-11 13:35:17 -0400

    Minor changes

---
+++ src/com/AdamWalters/com/zain/Level/tiles/SpawnPointTile.java
@@ -0,0 +1,22 @@
+package com.AdamWalters.com.zain.level.tiles;
+
+import com.AdamWalters.com.zain.graphics.Screen;
+import com.AdamWalters.com.zain.graphics.Sprite;
+
+public class SpawnPointTile extends Tile {
+
+	public SpawnPointTile(Sprite sprite) {
+		super(sprite);
+	}
+	
+	public void render(int x, int y, Screen screen) {
+		screen.renderTile(x << 4, y << 4, this);
+	}
+	
+	
+	/**
+	public boolean solid() {
+		return true;		<<--Redundant if left false. / This makes the Tile solid.
+	} **/
+
+}
---
+++ src/com/AdamWalters/com/zain/Level/tiles/spawn_level/SpawnSpawnPointTile.java
@@ -0,0 +1,27 @@
+package com.AdamWalters.com.zain.level.tiles.spawn_level;
+
+import com.AdamWalters.com.zain.graphics.Screen;
+import com.AdamWalters.com.zain.graphics.Sprite;
+import com.AdamWalters.com.zain.level.tiles.Tile;
+
+public class SpawnSpawnPointTile extends Tile {
+
+	public SpawnSpawnPointTile(Sprite sprite) {
+		super(sprite);
+	}
+	
+	public void render(int x, int y, Screen screen) {
+		screen.renderTile(x << 4, y << 4, this);
+	}
+
+	public boolean breakable() {
+		return true;
+	}
+	
+	
+	/**
+	public boolean solid() {
+		return true;		<<--Redundant if left false. / This makes the Tile solid.
+	} **/
+	
+}
